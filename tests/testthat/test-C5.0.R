bfa68246c50c453657121300f409362f28f65de0
Max Kuhn <mxkuhn@gmail.com>
2026-03-26 16:40:04 -0700

    bisection iter 5

--- tests/testthat/test-C5.0.R
+++ tests/testthat/test-C5.0.R
@@ -327,7 +327,7 @@ test_that("active_predictors.C5.0() handles tree with no splits", {
 
 test_that("active_predictors.C5.0() extracts from single boosted tree", {
   skip_if_not_installed("C50")
-
+  skip("troubleshooting")
 
   penguins <- get_penguins_data()
 
